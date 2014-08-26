9230287f33f0053e18ca3d604fb0a7712c64a3dc
thanawat.s <thanawats@englishguide.local>
2014-08-26 16:36:55 +0700

    Add new controller “AddCustomerViewController”

---
+++ MyCoreDataLearning/AddCustomerViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCustomerViewController.h
+//  MyCoreDataLearning
+//
+//  Created by thanawat.s on 8/26/2557 BE.
+//  Copyright (c) 2557 thanawat.s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCustomerViewController : UITableViewController
+
+@end
